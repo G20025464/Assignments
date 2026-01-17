2517029bff50309bf9549e8a953c1d5a16b7dde7
G20025464 <gokulpuri203@gmail.com>
2026-01-17 16:40:49 +0530

    Add files via upload

---
+++ assignment7/que2.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+void area(int);
+void main()
+{
+	int rad;
+	printf("Enter radius=");
+	scanf("%d",&rad);
+	area(rad);
+}//main ends here
+
+void area(int x)
+{
+	int*a=&x;
+	float res;
+	res=*a**a*3.14;
+	printf("Area of cirecle is %f",res);
+}
