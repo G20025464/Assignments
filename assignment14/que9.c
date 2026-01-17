3a0715eda3e37ad90b5f21dcbbddbeabc5a171be
G20025464 <gokulpuri203@gmail.com>
2026-01-17 16:47:16 +0530

    Add files via upload

---
+++ assignment14/que9.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+struct complex
+{
+    int real;
+    int imag;
+};
+
+void main()
+{
+    struct complex c1;
+
+    c1.real = 4;
+    c1.imag = 5;
+
+    printf("Complex Number = %d + %d", c1.real, c1.imag);
+}
